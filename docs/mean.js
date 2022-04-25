6743391eab4c99d35971c2526e1980f9fce75370
MzongziM <2270460825@qq.com>
2022-04-25 15:29:15 +0800

    调整参数

--- docs/mean.js
+++ docs/mean.js
@@ -2,12 +2,7 @@ window.onclick = function() {
 	var isChrome = window.navigator.userAgent.indexOf("Chrome") !== -1;
 	var audio = document.getElementById("audio");
 	if (isChrome) {
-		if (audio.paused) {
-			audio.play();
-			console.log("播放")
-		} else if (audio.played) {
-			audio.pause();
-			console.log("暂停");
-		}
+		audio.play();
+		console.log("播放")
 	}
 }
